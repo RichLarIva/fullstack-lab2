331d4452a0588efdf1c4ceb1eee0eae7759d577c
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-04-26 23:42:12 +0200

    Update to db.ts and index

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,19 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectDB = async () => 
-    {
-        try
-        {
-            const conn = await mongoose.connect(process.env.MONGODB_URI || '');
-            console.log(`MongoDB connected: ${conn.connection.host}`);
-        }
-        catch(err)
-        {
-            console.error(`Error: ${(err as Error).message}`);
-            process.exit(1);
-        }
-    }
-
+const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || '');
+    console.log(`✅ MongoDB successfully connected`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${(error as Error).message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
