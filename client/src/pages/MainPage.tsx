eb56081e1759fe793a71231e99bbe89ba01314dc
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-04-27 00:03:47 +0200

    Started adding pages

---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const MainPage = () => 
+{
+
+  return (
+    <>
+        <h1>Welcome to the Main Page</h1>
+        <p>This is the main page of the application.</p>
+        {/* Add more content or components as needed */}
+    </>
+  )
+}
+
+export default MainPage;
