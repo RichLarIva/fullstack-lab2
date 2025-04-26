078d2ecb80cb861196c2d545de9927a03c5e6ffb
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-04-26 23:52:58 +0200

    Added axios and services

---
+++ client/src/services/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// Set up Axios to connect to the backend
+const api = axios.create({
+    baseURL: `http://localhost:${PORT}/api/v1`,
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+
+export default api;
